951df6b2ce30228219273f9e1a4b39b856faa6c7
Mariia <mcherkashina77@gmail.com>
2023-12-02 16:33:07 +0100

    added styles for setting form

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useRef } from "react";
 import { ReactComponent as UploadImgSvg } from "../../images/svg/upload_icon.svg";
+import { ReactComponent as CloseModalSvg } from "../../images/svg/x.svg";
 import SettingForm from "./SettingForm";
+import {
+  ContainerSettings,
+  UpElementsWrapper,
+  UploadWrapper,
+} from "./SettingModal.styled";
 
 const SettingModal = () => {
   const filePickerRef = useRef(null);
@@ -10,32 +16,40 @@ const SettingModal = () => {
     filePickerRef.current.click();
   };
 
+  const FileChange = (e) => {
+    const formaData = new FormData();
+    console.log(formaData);
+    // dispatch to update avatar will be here
+  };
+
   return (
-    <div>
-      <div>
+    <ContainerSettings>
+      <UpElementsWrapper>
         <h2>Setting</h2>
-        <h3>Your photo</h3>
+        <CloseModalSvg />
+      </UpElementsWrapper>
+      <h3>Your photo</h3>
+      <UploadWrapper>
         <div>
           <img src={"avatar"} alt="avatar" />
-          <label>
-            <input
-              style={{ display: "none" }}
-              ref={filePickerRef}
-              type="file"
-              accept=".jpg"
-              //   onChange={FileChange}
-              // dispatch to update avatar will be here
-            />
-            <button type="button" onClick={handleButtonClick}>
-              <UploadImgSvg />
-              <span>Upload a photo</span>
-            </button>
-          </label>
-          {selectedFile && <img src={selectedFile} alt="Picked Image" />}
         </div>
-        <SettingForm />
-      </div>
-    </div>
+        <label>
+          <input
+            style={{ display: "none" }}
+            ref={filePickerRef}
+            type="file"
+            accept=".jpg"
+            onChange={FileChange}
+          />
+          <button type="button" onClick={handleButtonClick}>
+            <UploadImgSvg />
+            Upload a photo
+          </button>
+        </label>
+        {selectedFile && <img src={selectedFile} alt="Picked Image" />}
+      </UploadWrapper>
+      <SettingForm />
+    </ContainerSettings>
   );
 };
 
