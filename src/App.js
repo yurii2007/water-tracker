9704ccb12ecf64d051f6f9e5febf5453395a349d
yurii2007 <yurash862@gmail.com>
2023-12-02 10:32:03 +0200

    added layout to both auth pages and shared layout to all pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import WelcomePage from "./pages/WelcomePage";
+import AuthLayout from "./components/AuthLayout/AuthLayout";
 
 const HomePage = React.lazy(() => import("./pages/HomePage"));
 const SignInPage = React.lazy(() => import("./pages/SignInPage"));
@@ -15,8 +16,24 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<HomePage />} /> {/* Private route */}
         <Route path="/" element={<WelcomePage />} /> {/* Public route */}
-        <Route path="/signin" element={<SignInPage />} /> {/* Public route */}
-        <Route path="/signup" element={<SignUpPage />} /> {/* Public route */}
+        <Route
+          path="/signin"
+          element={
+            <AuthLayout>
+              <SignInPage />
+            </AuthLayout>
+          }
+        />
+        {/* Public route */}
+        <Route
+          path="/signup"
+          element={
+            <AuthLayout>
+              <SignUpPage />
+            </AuthLayout>
+          }
+        />
+        {/* Public route */}
       </Route>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
