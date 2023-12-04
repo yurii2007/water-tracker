c235a241d260efd0c430537d94db1d310e4c82b4
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-12-04 13:45:35 +0200

    Welcome page
    components on welcome page

---
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,20 @@
+import Container from "../../css/Container";
+import { Wrapper } from "./WelcomePage.styled";
+import WelcomeTracker from "../../components/WaterTracker";
+import WhyDrinkWater from "../../components/WhyDrinkWater";
+import { WelcomeWrapper } from "./WelcomePage.styled";
+
+const WelcomePage = () => {
+  return (
+    <Wrapper>
+      <Container>
+        <WelcomeWrapper>
+          <WelcomeTracker />
+          <WhyDrinkWater />
+        </WelcomeWrapper>
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default WelcomePage;
