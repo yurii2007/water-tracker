e965d0f69d045bd2c65b239bc0ff221463558c09
Mariia <mcherkashina77@gmail.com>
2023-12-05 19:33:46 +0100

    work on form

--- src/schemas/settingFormSchema.js
+++ src/schemas/settingFormSchema.js
@@ -1,22 +1,21 @@
-import * as Yup from "yup";
+import * as yup from "yup";
+const emailRegex = /[a-z0-9]+@+[a-z]+[\./]+[a-z]{2,3}/;
 
-export const validationSchema = Yup.object().shape({
-  gender: Yup.string().required("Gender is required"),
-  name: Yup.string().required("Name is required"),
-  email: Yup.string()
-    .trim()
-    .email("Invalid email address")
-    .matches(
-      /^[a-zA-Z0-9]+[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9]+$/,
-      "email is not valid"
+export const validationSchema = yup.object().shape({
+  gender: yup.string().required(),
+  name: yup.string(),
+  email: yup.string().matches(emailRegex, "Email is not valid"),
+  oldPassword: yup
+    .string()
+    .when("newPassword", (newPassword, field) =>
+      newPassword[0] ? field.required() : field
     ),
-  oldPassword: Yup.string(),
-  newPassword: Yup.string(),
-  repeatPassword: Yup.string().when("newPassword", {
-    is: (val) => val && val.length > 0,
-    then: Yup.string().oneOf(
-      [Yup.ref("newPassword"), null],
-      "Passwords must match"
-    ),
-  }),
+  newPassword: yup.string().nullable(),
+
+  repeatPassword: yup
+    .string()
+    .test("commonPassword", "Passwords do not match.", function (value) {
+      const newPassword = this.resolve(yup.ref("newPassword"));
+      return !newPassword || String(value) === String(newPassword);
+    }),
 });
