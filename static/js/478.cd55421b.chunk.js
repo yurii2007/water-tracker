a966f6b2cb71293441ea679d9c65df54fca91523
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-12-09 14:30:03 +0000

    Deploying to gh-pages from @ yurii2007/water-tracker@c523a456c706f9410a1ca8cbc0bf58e57e6d9832 🚀

---
+++ static/js/478.cd55421b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwater_tracker=self.webpackChunkwater_tracker||[]).push([[478],{3478:(e,t,r)=>{r.r(t),r.d(t,{default:()=>w});var a=r(1087),n=r(7689),c=r(9434),s=r(2791),u=r(8155),k=r(4905),f=r(3359),h=r(2785),o=r(3329);const w=()=>{const[e]=(0,a.lr)(),t=e.get("token"),r=(0,n.s0)(),w=(0,c.I0)();return(0,s.useEffect)((()=>{(()=>{if(!t)return r(u._.SignIn);w((0,k.a)(t)),w((0,f.eC)()).unwrap().then((()=>{r(u._.Home)})).catch((()=>{r(u._.Home)}))})()}),[w,r,t]),(0,o.jsx)(h.Z,{})}}}]);
+//# sourceMappingURL=478.cd55421b.chunk.js.map
