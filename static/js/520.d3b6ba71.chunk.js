f354357e8e3fab50ab5a579281108d1d4fe563d1
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-12-13 12:34:31 +0000

    Deploying to gh-pages from @ yurii2007/water-tracker@3a697deed5459d3f27645f48c249a8903a5654a2 🚀

--- static/js/520.d3b6ba71.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkwater_tracker=self.webpackChunkwater_tracker||[]).push([[520],{4520:(e,s,a)=>{a.r(s),a.d(s,{default:()=>l});var r=a(2791),c=a(7689),t=a(9970),k=a(3329);const l=()=>(0,k.jsx)(r.Suspense,{fallback:(0,k.jsx)(t.Z,{}),children:(0,k.jsx)(c.j3,{})})}}]);
-//# sourceMappingURL=520.d3b6ba71.chunk.js.map
